083f94acfc23ee0174a42e8b1d1906675a5a7535
Namgyu11 <hagaji3@naver.com>
2023-12-13 20:24:47 +0900

    프론트 배너 노출 기능

--- src/main/java/com/zerobase/fastlms/main/controller/MainController.java
+++ src/main/java/com/zerobase/fastlms/main/controller/MainController.java
@@ -1,36 +1,30 @@
 package com.zerobase.fastlms.main.controller;
 
 
-import com.zerobase.fastlms.components.MailComponents;
+import com.zerobase.fastlms.admin.dto.BannerDto;
+import com.zerobase.fastlms.admin.model.BannerParam;
+import com.zerobase.fastlms.admin.service.BannerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 @Slf4j
 public class MainController {
 
-    private final MailComponents mailComponents;
-    
+    private final BannerService bannerService;
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model, BannerParam parameter) {
 
+        List<BannerDto> bannerDtoList = bannerService.showList(parameter);
+        model.addAttribute("list", bannerDtoList);
 
-        String email = "satcop@naver.com";
-        String subject = " 안녕하세요. 제로베이스 입니다. ";
-        String text = "<p>안녕하세요.</p><p>반갑습니다.</p>";
-
-        mailComponents.sendMail(email, subject, text);
-
-        
         return "index";
     }
     
